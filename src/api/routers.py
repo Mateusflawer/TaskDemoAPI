056e7c367d1c4e7799f79526be0f92902fa052ab
Mateus Menezes <111348801+Mateusflawer@users.noreply.github.com>
2025-06-03 13:43:08 -0300

    feat: update login response to include username and modify Token schema

--- src/api/routers.py
+++ src/api/routers.py
@@ -9,6 +9,7 @@ api_router = APIRouter()
 # Inclui roteadores de tarefas
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 
+
 # Endpoint para login
 @api_router.post("/token", response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -20,4 +21,4 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": user.username})
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"username": user.username, "access_token": access_token}
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class Token(BaseModel):
+    username: str
     access_token: str
-    token_type: str
